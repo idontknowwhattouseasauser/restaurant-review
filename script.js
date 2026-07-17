f967048ca40a24fe9e40243ad584eb2d668f087a
idontknowwhattouseasauser <kh_pr@hotmail.com>
2026-07-17 19:36:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+class Shop{
+    constructor(reviewer,shop,type,rate,message){
+        this.reviewer=reviewer;
+        this.shop=shop;
+        this.type=type;
+        this.rate=rate;
+        this.message=message;
+    }
+}
+
+
+class UI{
+    static addshoptolist=(shop-form)=>{
+        const list=document.getElementById("shop-list");
+        const row=document.createElement("tr")
+        row.innerHTML=`<td>${food.reviewer}</td>
+        <td>${food.shop}</td>
+        <td>${food.type}</td>
+        <td>${food.rate}</td>
+        <td>${food.message}</td>`
+        list.appendChild(row)
+    }
+    static displayshops=()=>{
+        default_shops.forEach(shops => UI.addshoptolist(shop)
+            
+        );
+    }
+    
+}
+
+UI.displayshops()
+document.querySelector("#shop-form").addEventListener("submit",addashop)
+function addashop(e){
+    console.log("formSubmitted");
+    e.preventDefault()
+    const yn=document.querySelector("#yn").value;
+    const rn=document.querySelector("#rn").value;
+    const type=document.querySelector("#type").value;
+    const rate=document.querySelector("#rate").value;
+    const mr=document.querySelector("#mr").value;
+    const shop=new Shop(yn, rn, type, rate, mr)
+    UI.addshoptolist(shop)
+    
+}
